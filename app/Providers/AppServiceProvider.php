8ebaed343d8cef64de8e22c921f6bb426e8e3637
David Galdamez <david.galdamez2014@gmail.com>
2025-02-17 16:59:29 -0600

    no funciono otra vez

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind('path_public', 'html/paqueteria');
+        //
     }
 
     /**
